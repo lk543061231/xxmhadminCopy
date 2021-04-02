d048511f6ff6716bf02af66d0c007dc8df026aa0
kui wang <kuiwang@wholeren.com>
2021-04-02 16:52:43 +0800

    new

--- src/store/index.js
+++ src/store/index.js
@@ -5,23 +5,18 @@ import app from './modules/app'
 import user from './modules/user'
 import permission from './modules/permission'
 import getters from './getters'
-
+import createLogger from 'vuex/dist/logger'
 Vue.use(Vuex)
-
+const isProd = process.env.NODE_ENV === 'production'
 export default new Vuex.Store({
   modules: {
     app,
     user,
     permission
   },
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
-  getters
+  state: {},
+  mutations: {},
+  actions: {},
+  getters,
+  plugins: isProd ? [] : [createLogger()]
 })
